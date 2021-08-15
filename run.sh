ef832a342bada2f5bad9fdab0f3de77cb793bc29
Fran Simó <fransimo@gmail.com>
2021-08-15 19:36:47 +0200

    video.html, no cursor

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-/usr/bin/vlc --fullscreen --loop /home/pi/selfie_v2/2016_05_27_pd_t2_tse_tra.mp4
-
-#/usr/bin/chromium-browser --start-fullscreen file:///home/pi/selfie_v2/2016_05_27_t2_fl2d_cor.gif
-
+# /usr/bin/vlc --fullscreen --loop /home/pi/selfie_v2/output.mp4
 
 
 # https://raspberrypi.stackexchange.com/questions/9180/software-for-displaying-animated-gifs
 #/usr/bin/animate file:///home/pi/selfie_v2/2016_05_27_t2_fl2d_cor.gif
 
-
-
-
 #  convert -background none -rotate 90 -coalesce -delay 10x100  0_2016_05_27_t2_fl2d_cor.gif 2016_05_27_t2_fl2d_cor.gif 
+
 /usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/2016_05_27_t2_fl2d_cor.gif
+
+# /usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/output.mp4
+
+/usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/video.html
 
 
 # https://learn.adafruit.com/raspi-animated-gif-picture-frame?view=all
