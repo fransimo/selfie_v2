9edb9c5c874dfa6a9f8257aaea74ab6f7e4c9bb0
Fran Simó <fransimo@gmail.com>
2021-09-04 19:56:37 +0200

    First on-site cahnges

---
+++ convert_2_on-site.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export SOURCE="originals_gif"
+export DESTINATION="on-site/videos"
+export RESIZE="480x257"
+export DELAY="5x100"
+
+convert -background none -rotate 270 -resize ${RESIZE} -delay ${DELAY} ${SOURCE}/2016_05_27_t2_fl2d_cor.gif ${DESTINATION}/01_00_2016_05_27_t2_fl2d_cor.gif
+
+convert -background none -rotate 270 -resize ${RESIZE} -delay ${DELAY} ${SOURCE}/2016_05_27_pd_t2_tse_tra.gif ${DESTINATION}/01_01_2016_05_27_pd_t2_tse_tra.gif
+
+convert -background none -rotate 270 -resize ${RESIZE} -delay ${DELAY} ${SOURCE}/2016_05_27_t1_tse_cor.gif ${DESTINATION}/02_00_2016_05_27_t1_tse_cor.gif
+
+convert -background none -rotate 270 -resize ${RESIZE} -delay ${DELAY} ${SOURCE}/2016_05_27_t2_tse_fs_tra_256.gif ${DESTINATION}/02_01_2016_05_27_t2_tse_fs_tra_256.gif
--- run.sh
+++ run.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
+export URL=file://${HOME}/selfie_v2/on-site/index.html
 
-# /usr/bin/vlc --fullscreen --loop /home/pi/selfie_v2/output.mp4
-
-
-# https://raspberrypi.stackexchange.com/questions/9180/software-for-displaying-animated-gifs
-#/usr/bin/animate file:///home/pi/selfie_v2/2016_05_27_t2_fl2d_cor.gif
-
-#  convert -background none -rotate 90 -coalesce -delay 10x100  0_2016_05_27_t2_fl2d_cor.gif 2016_05_27_t2_fl2d_cor.gif 
-
-/usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/2016_05_27_t2_fl2d_cor.gif
-
-# /usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/output.mp4
-
-# /usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/video.html
-
-
-# https://learn.adafruit.com/raspi-animated-gif-picture-frame?view=all
-#/usr/bin/chromium-browser --noerrdialogs --kiosk file:///home/pi/selfie_v2/gif_player.html
-
-
-
- # no funciona /usr/bin/dillo -f file:///home/pi/selfie_v2/2016_05_27_t2_fl2d_cor.gif
+/usr/bin/chromium-browser --noerrdialogs --kiosk ${URL}
