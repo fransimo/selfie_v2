966286f6be0beeb3ddd15f24c2d909ee34ef0f91
Fran Simó <fransimo@gmail.com>
2021-09-19 15:28:18 +0200

    hosts names

--- install.sh
+++ install.sh
@@ -2,16 +2,14 @@
 
 # Change host name 
 
-export newname=$(cat /sys/class/net/eth0/address | tr -d ":") 
+export newname=selfie-$(cat /sys/class/net/eth0/address | tr -d ":") 
 echo $newname
 
 echo "127.0.1.1	$newname" | sudo tee -a /etc/hosts
 echo "$newname" | sudo tee /etc/hostname
 sudo hostname $newname
 
-exit 0
-
-export oldname=$(cat /proc/sys/kernel/hostname)
-echo $oldname
+# export oldname=$(cat /proc/sys/kernel/hostname)
+# echo $oldname
 # sudo sed -i "s/${oldname}/${newname}/" /etc/hosts
 
