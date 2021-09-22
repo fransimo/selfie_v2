ae5a56884f86c9e0e2fffbc0286d871f670ec130
Fran Simó <fransimo@gmail.com>
2021-09-22 19:22:27 +0200

    check names

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 export URL=http://localhost:8000/
 
-cd ${HOME}/selfie_v2/on-site/
+cd ${HOME}/selfie_v2
+
+# System
 
 git pull
+
+./check-names.sh
+
+# Kiosk
+
+cd on-site
 
 python3 -m http.server &
 
