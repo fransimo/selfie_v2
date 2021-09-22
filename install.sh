c9838822d8e72ba8b2e6e1fe49acc62f1d040f3b
Fran Simó <fransimo@gmail.com>
2021-09-22 19:18:41 +0200

    check names

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@
 export newname=selfie-$(cat /sys/class/net/eth0/address | tr -d ":") 
 echo $newname
 
-echo "127.0.1.1	$newname" | sudo tee -a /etc/hosts
-echo "$newname" | sudo tee /etc/hostname
-sudo hostname $newname
+export currentname=$(cat /proc/sys/kernel/hostname)
+echo $currentname
 
-# export oldname=$(cat /proc/sys/kernel/hostname)
-# echo $oldname
-# sudo sed -i "s/${oldname}/${newname}/" /etc/hosts
-
+if [ ! "$newname" == "$currentname" ] ; then
+	echo "Name is different"
+	echo "127.0.1.1	$newname" | sudo tee -a /etc/hosts
+	echo "$newname" | sudo tee /etc/hostname
+	sudo hostname $newname
+	# sudo sed -i "s/${currentname}/${newname}/" /etc/hosts
+else
+	echo "Name is OK"	
+fi
