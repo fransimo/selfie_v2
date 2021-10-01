a144f79d473ba004563373e447f487b63996c77c
Fran Simó <fransimo@gmail.com>
2021-10-01 18:36:49 +0200

    shutdown

---
+++ shutdown.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1
+ssh pi@selfie-b827eb81d4a2.local "sudo poweroff"
+
+# 2
+ssh pi@selfie-b827eb45cb8e.local "sudo poweroff"
+
+# 3
+ssh pi@selfie-b827eb8fa9fd.local "sudo poweroff"
+
+# 4
+ssh pi@selfie-b827eb664d68.local "sudo poweroff"
+
+# 5
+ssh pi@selfie-b827eb7fe07d.local "sudo poweroff"
+
+# 0
+ssh pi@selfie-e45f015bee2e.local "sudo poweroff"
