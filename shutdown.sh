378dc92c87ef1f7e57e4f9cdce6768f0664498c3
Fran Simó <fransimo@gmail.com>
2021-10-03 16:01:00 +0200

    auto update

--- shutdown.sh
+++ shutdown.sh
@@ -8,19 +8,19 @@ if [ "$1" != "" ] ; then
 fi
 
 # 1
-ssh pi@selfie-b827eb81d4a2.local "sudo poweroff"
+ssh pi@selfie-b827eb81d4a2.local "$command"
 
 # 2
-ssh pi@selfie-b827eb45cb8e.local "sudo poweroff"
+ssh pi@selfie-b827eb45cb8e.local "$command"
 
 # 3
-ssh pi@selfie-b827eb8fa9fd.local "sudo poweroff"
+ssh pi@selfie-b827eb8fa9fd.local "$command"
 
 # 4
-ssh pi@selfie-b827eb664d68.local "sudo poweroff"
+ssh pi@selfie-b827eb664d68.local "$command"
 
 # 5
-ssh pi@selfie-b827eb7fe07d.local "sudo poweroff"
+ssh pi@selfie-b827eb7fe07d.local "$command"
 
 # 0
-ssh pi@selfie-e45f015bee2e.local "sudo poweroff"
+ssh pi@selfie-e45f015bee2e.local "$command"
