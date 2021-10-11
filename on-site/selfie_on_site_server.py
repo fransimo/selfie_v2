a24eb89815d3dcea0b5eb60b57b5718a1b778431
Fran Simó <fransimo@gmail.com>
2021-10-11 17:13:35 +0200

    grayscale(1) contrast(150%) not being applied

--- on-site/selfie_on_site_server.py
+++ on-site/selfie_on_site_server.py
@@ -2,15 +2,17 @@ from flask import Flask
 from flask import request
 
 import base64
-
+from PIL import Image, ImageOps
+from io import BytesIO
 
 import socket
 
 def convert_and_save(b64_string):
     imgdata = base64.b64decode(b64_string)
-    # imgdata = b64_string
-    with open("selfie.png", "wb") as fh:
-        fh.write(imgdata)
+    # im = ImageOps.grayscale(Image.open(BytesIO(imgdata)))
+    im = Image.open(BytesIO(imgdata))
+    im.save('image.png', 'PNG')
+
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -20,8 +22,6 @@ def hello_world():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    print(request)
-    print(request.files)
     if request.method == 'POST':
         json_data = request.get_json()
         convert_and_save(json_data['selfie'])
---
+++ updates/06/update_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+
+cd ${HOME}/selfie_v2/on-site
+source venv/bin/activate
+
+pip install Pillow
