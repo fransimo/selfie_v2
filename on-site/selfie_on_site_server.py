51ce38c89520c2e47f9f8fa6ad40fc59768001be
Fran Simó <fransimo@gmail.com>
2021-10-29 22:07:41 +0200

    qr on static, delete file

--- on-site/selfie_on_site_server.py
+++ on-site/selfie_on_site_server.py
@@ -8,15 +8,7 @@ from PIL import ImageGrab
 from datetime import datetime
 from hashlib import md5
 
-def upload_file(file_name):
-    try:
-        scp = subprocess.Popen(['scp', file_name, 'dh_ep9wdy@selfie-v2.fransimo.info:/home/dh_ep9wdy/selfie-v2.fransimo.info/public/snap_shots/'])
-        pid, status = os.waitpid(scp.pid, 0)
-        print(status)
-        return ( status == 0 )
-    except CalledProcessError:
-        print('ERROR: Connection to host failed!')
-        return False
+import qrcode
 
 def take_snap_shot():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -28,12 +20,32 @@ def take_snap_shot():
     img.save(file_name)
     return file_name
 
+
+def upload_file(file_name):
+    try:
+        scp = subprocess.Popen(['scp', file_name, 'dh_ep9wdy@selfie-v2.fransimo.info:/home/dh_ep9wdy/selfie-v2.fransimo.info/public/snap_shots/'])
+        pid, status = os.waitpid(scp.pid, 0)
+        print(status)
+        return ( status == 0 )
+    except CalledProcessError:
+        print('ERROR: Connection to host failed!')
+        return False
+
+def generate_qr(file_name):
+    url = 'https://www.selfie-v2.fransimo.info/'+file_name
+    img = qrcode.make(url)
+    qr_uri = 'static/' + file_name + '.png'
+    img.save(qr_uri)
+    return qr_uri
+
+
 def take_snap_shot_and_upload():
     file_name = take_snap_shot()
     if upload_file(file_name):
-        r = 'https://www.selfie-v2.fransimo.info/'+file_name
+        r = 'http://localhost:5000/' + generate_qr(file_name)
     else:
         r = "not online, sorry!"
+    os.remove(file_name)
     return r
 
 app = Flask(__name__, static_url_path='/static')
