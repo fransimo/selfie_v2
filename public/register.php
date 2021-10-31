0e703c1b4015b08bf24b7d462a592c1283c1afa2
Fran Simó <fransimo@gmail.com>
2021-10-31 18:41:10 +0100

    archive

--- public/register.php
+++ public/register.php
@@ -1,11 +1,13 @@
 <?php
   $name=$_GET["name"];
   $email=$_GET["email"];
+  $archive=$_GET["email"];
   $file=$_GET["file"];
-  $arch_file=str_replace('snap_shots','archive',$file)
-  copy($file, $arch_file)
-  $arch_meta  = fopen(arch_file . '.dat', "w")
-  $txt = $name + "\n" . $email . "\n" . $file;
+
+  $arch_file=str_replace('snap_shots','archive',$file);
+  copy($file, $arch_file);
+  $arch_meta  = fopen($arch_file . '.dat', "w");
+  $txt = $name . "\n" . $email . "\n" . $archive . "\n" . $file;
   fwrite($arch_meta, $txt);
   fclose($arch_meta);
 ?>
