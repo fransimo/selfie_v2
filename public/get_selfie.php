2771b85de676f70ee56352e646e0e2cc78fdf6dc
Fran Simó <fransimo@gmail.com>
2021-10-31 16:12:55 +0100

    download

--- public/get_selfie.php
+++ public/get_selfie.php
@@ -2,10 +2,15 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Selfie_v2</title>
+    <title>Selfie_v2 - download</title>
     <link href="style.css" rel="stylesheet">
   </head>
   <body>
-    <img src="<?php echo $_GET["id"]; ?>" />
+    <main>
+      <a download="selfie_v2.jpg" href="<?php echo $_GET["id"]; ?>" title="Selfie_v2">
+        <p>Click to download!</p>
+        <img alt="Selfie_v2" src="<?php echo $_GET["id"]; ?>">
+      </a>
+    </main>
   </body>
 </html>
