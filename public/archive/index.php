0fd09d61a4ad142e196e18827736fa2567bcb216
Fran Simó <fransimo@gmail.com>
2021-11-02 20:03:27 +0100

    disclaimer

--- public/archive/index.php
+++ public/archive/index.php
@@ -8,12 +8,17 @@
   </head>
   <body>
     <main>
-      <p>Text</p>
+      <h1>Archivo #Selfie_v2</h1>
+      <ul>Las personas que decidan participar en el archivo aceptan que el autor:</ul>
+         <li>Publique las imágenes en esta web solo durante la exposición en ideoGrama.</li>
+         <li>Contactar por email para enviar noticias sobre su obra.</li>
+         <li>En caso de nuevos usuos siempre se pedirá permiso explicito.</li>
+      </ul>
+
       <?php
         $fileList = glob('*.jpg');
 
         foreach($fileList as $filename){
-           //Simply print them out onto the screen.
            echo '<p><img alt="Selfie_v2" src="'.$filename.'"></p>';
         }
       ?>
