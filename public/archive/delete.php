522b3b818dfffbe692ea662fda8a91d8e6021da7
Fran Simó <fransimo@gmail.com>
2021-11-03 09:33:22 +0100

    delete

---
+++ public/archive/delete.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Selfie_v2 Archive</title>
+    <meta content="width=device-width, initial-scale=1, user-scalable=no" name="viewport"/>
+    <link href="../style.css" rel="stylesheet">
+  </head>
+  <body>
+    <main>
+      <p>Disponible pronto. Disculpad las molestias.</p>
+      <?php
+        //Mostrar formulario si no viene el email informado.
+        //Si coinciden borrar la imagen y redirigie al archivo.
+      ?>
+    </main>
+  </body>
+</html>
